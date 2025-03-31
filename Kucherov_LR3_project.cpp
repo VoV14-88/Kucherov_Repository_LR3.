719e464cb933861e70d4a2117fabae2a973665cd
VoV14-88 <kucerovvova36@gmail.com>
2025-03-31 21:22:57 +0300

    create void main

---
+++ Kucherov_LR3_project.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int foo1(int a)
+{
+ // в разработке
+}
+
+int foo3(int b)
+{
+ // в разработке
+}
+
+int foo4(int b)
+{
+ // в разработке
+}
+
+int foo5(int b)
+{
+ // в разработке
+}
+
+int main()
+{
+
+}
